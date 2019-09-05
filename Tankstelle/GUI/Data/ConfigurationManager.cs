1625bcb695fb3883e11d48d47c0b9a12f6e904ee
Noah Emmenegger <noahemmenegger.business@gmail.com>
2019-09-05 19:14:37 +0200

    add data

---
+++ Tankstelle/GUI/Data/ConfigurationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Data
+{
+    class ConfigurationManager
+    {
+        private string filePath = "";
+        private int NumberOfGasStation;
+
+        public int GetNumberOfGasStation()
+        {
+
+            return 2;
+        }
+
+        public void GetDataAsJson()
+        {
+            StreamReader sr = new StreamReader(filePath);
+        }
+        public void SaveChanges()
+        {
+            StreamWriter sw = new StreamWriter(filePath);
+        }
+    }
+}
