b6075587b9d9f6fdf062211e3b94b2370be3afad
Nicolas Bürgler <trajan15.nb@gmail.com>
2019-09-09 11:46:27 +0200

    Diverse Änderungen

--- Tankstelle/GUI/Business/GasPump.cs
+++ Tankstelle/GUI/Business/GasPump.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tankstelle.Enums;
 
 namespace Tankstelle.Business
 {
     public class GasPump
     {
+        private bool _isLocked = false;
+        public int GasPumpNumber { get; set; }
+        public List<Fuel> FuelList { get; set; }
+        public decimal ToPayValue { get; set; }
+        public ActiveFuelPropertys ActiveFuel { get; set; }
+        public void OpenDisplay()
+        {
 
+        }
     }
 }
