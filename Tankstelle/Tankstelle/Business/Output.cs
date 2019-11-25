b7707e3c46f6d02270d20c5f160a1c1b24e41f07
Nicolas Bürgler <trajan15.nb@gmail.com>
2019-11-25 12:01:35 +0100

    Klasse Output hinzugefügt

---
+++ Tankstelle/Tankstelle/Business/Output.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tankstelle.Business
+{
+    public static class Output
+    {
+        public static void WriteException(Exception ex)
+        {
+
+        }
+
+        public static void WriteWarning(Exception ex)
+        {
+
+        }
+
+        public static void WriteInfo(Exception ex)
+        {
+
+        }
+
+        public static void WriteException(string name, string description)
+        {
+            
+        }
+
+        public static void WriteWarning(string name, string description)
+        {
+
+        }
+
+        public static void WriteInfo(string name, string description)
+        {
+
+        }
+    }
+}
