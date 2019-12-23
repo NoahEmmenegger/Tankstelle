768a3de528d1b960470f6b0c33870df1a3e02d4d
Noah Emmenegger <noahemmenegger.business@gmail.com>
2019-12-23 14:38:40 +0100

    WIP: Statistic

--- Tankstelle/Tankstelle/GUI/Admin/StatisticDisplay.xaml.cs
+++ Tankstelle/Tankstelle/GUI/Admin/StatisticDisplay.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Tankstelle.Business;
+using Tankstelle.Data;
 
 namespace Tankstelle.GUI.Admin
 {
@@ -19,9 +21,42 @@ namespace Tankstelle.GUI.Admin
     /// </summary>
     public partial class StatisticDisplay : Window
     {
+        IConfigurationManager configurationManager = ConfigurationManager.CreateInstance();
         public StatisticDisplay()
         {
             InitializeComponent();
+            tanklist.ItemsSource = configurationManager.GetTanks();
+
+            Statistic statistic = new Statistic();
+            statistic.Umsatz = "test";
+            statistic.Monat = monat.SelectedIndex;
+
+            this.DataContext = statistic;
+
+
+            
         }
+    }
+
+    public class Statistic
+    {
+        public int Monat { get; set; }
+        public int Einnahmen { 
+            get
+            {
+                IConfigurationManager configurationManager = ConfigurationManager.CreateInstance();
+                int einnahmen = 0;
+                foreach (Receipt receipt in configurationManager.GetReceipts())
+                {
+                    if (receipt.Date.Month == 12)
+                    {
+                        einnahmen += receipt.Sum;
+                    }
+                }
+                return 0;
+            }
+        }
+        public int Ausgaben { get; set; }
+        public string Umsatz { get; set; }
     }
 }
