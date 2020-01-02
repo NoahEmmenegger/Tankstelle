75bd77aabb97e1a7ad3558b0df0d736bebe9c335
Noah Emmenegger <noahemmenegger.business@gmail.com>
2020-01-02 15:47:18 +0100

    add Heute to statistik

--- Tankstelle/Tankstelle/GUI/Admin/StatisticDisplay.xaml.cs
+++ Tankstelle/Tankstelle/GUI/Admin/StatisticDisplay.xaml.cs
@@ -25,7 +25,48 @@ namespace Tankstelle.GUI.Admin
         public StatisticDisplay()
         {
             InitializeComponent();
-            tanklist.ItemsSource = configurationManager.GetTanks();           
+            List<FuelStatistic> fuelStatistics = new List<FuelStatistic>();
+            foreach (Fuel fuel in configurationManager.GetFuels())
+            {
+                FuelStatistic fuelStatistic = new FuelStatistic();
+                fuelStatistic.Fuel = fuel;
+                fuelStatistic.SoldLiters = GetSoldLiters(DateTime.Now, fuel);
+                fuelStatistic.Earnings = GetFuelEarnings(fuel, DateTime.Now);
+                fuelStatistics.Add(fuelStatistic);
+            }
+            tanklist.ItemsSource = configurationManager.GetTanks();
+            treibstoffsorten.ItemsSource = fuelStatistics;
+            DayEarning.Text = "Umsatz: " + GetDayEarning(DateTime.Now);
+        }
+
+        public int GetDayEarning(DateTime date)
+        {
+            int earnings = 0;
+            foreach (Receipt receipt in configurationManager.GetReceipts().Where(x => x.Date.Date == date.Date))
+            {
+                earnings += receipt.Sum;
+            }
+            return earnings;
+        }
+
+        public float GetSoldLiters(DateTime date, Fuel fuel)
+        {
+            float litercount = 0;
+            foreach (Receipt receipt in configurationManager.GetReceipts().Where(x => x.Date.Date  == date.Date && x.RelatedFuel == fuel))
+            {
+                litercount += receipt.RelatedLiter;
+            }
+            return litercount;
+        }
+
+        public decimal GetFuelEarnings(Fuel fuel, DateTime date)
+        {
+            decimal earnings = 0;
+            foreach (Receipt receipt in configurationManager.GetReceipts().Where(x => x.Date.Date == date.Date && x.RelatedFuel == fuel))
+            {
+                earnings += receipt.Sum;
+            }
+            return earnings;
         }
 
         public int GetEarnings(int month)
@@ -52,16 +93,25 @@ namespace Tankstelle.GUI.Admin
             statistic.Earnings = this.GetEarnings(monat.SelectedIndex);
             statistic.Outgoings = this.GetOutgoings(monat.SelectedIndex);
             statistic.MetabolicRate = statistic.Earnings - statistic.Outgoings;
-
+            
             this.DataContext = statistic;
+            treibstoffsorten.ItemsSource = configurationManager.GetFuels();
         }
     }
 
+    //Alle Klassen noch entfernen
     public class Statistic
     {
         public int Monat { get; set; }
         public int Earnings { get; set; }
         public int Outgoings { get; set; }
         public int MetabolicRate { get; set; }
+    }
+
+    public class FuelStatistic
+    {
+        public Fuel Fuel { get; set; }
+        public float SoldLiters { get; set; }
+        public decimal Earnings { get; set; }
     }
 }
